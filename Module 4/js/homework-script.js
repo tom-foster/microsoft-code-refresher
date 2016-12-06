8dd9b4d2f60710a6c3d9e17775e0673239935173
tom-foster <tom-foster@users.noreply.github.com>
2016-12-06 23:07:39 +0000

    make a carousel

---
+++ Module 4/js/homework-script.js
@@ -0,0 +1,32 @@
+// Carousel script
+var slideInterval = 3500;
+console.log("hello");
+
+function getFigures () {
+    return document.getElementById('carousel').getElementsByTagName('figure');
+}
+
+function moveForward() {
+    var pointer;
+    var figures = getFigures();
+    for (var i = 0; i < figures.length; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = '';
+            pointer = i;
+        }
+    }
+    if (++pointer == figures.length) {
+        pointer = 0;
+    }
+
+    figures[pointer].className = 'visible';
+    setTimeout(moveForward, slideInterval);
+}
+
+function startPlayback() {
+   setTimeout(moveForward, slideInterval);
+}
+
+startPlayback();
+
+console.log("hello");
